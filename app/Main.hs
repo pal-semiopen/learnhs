0ce8fa7d7408f0bb5f50ab1f5fdc2fc1b97d47d8
Koharu Mikumo <otameshiiiiii@otameshi.com>
2021-02-27 18:52:17 +0900

    バグってるけどコンパイル通った

--- app/Main.hs
+++ app/Main.hs
@@ -15,15 +15,15 @@ data StrTree = Tree [StrTree] | Chr Char
 
 --split_by_close のサポート
 s_b_c' :: (String->Maybe(String,String)) -> Char -> String -> Maybe(String, String)
-s_b_c' = \f c s -> case f s of
-  Nothing -> Nothing
-  Just xxx -> case c of
-    ')' -> (Just([], s))
-    x   -> (Just(  (x:(fst xxx)) , (snd xxx)  ))
+s_b_c' = \f c s -> case c of
+  ')' -> (Just([], s))
+  x   -> case f s of
+    Nothing -> Nothing
+    Just xxx -> (Just(  (x:(fst xxx)) , (snd xxx)  ))
 
 split_by_close ::String -> Maybe(String, String)
 split_by_close = \s -> case s of
-    []   -> Nothing
+    []   -> Nothing --Just([], [])
     c:s' -> s_b_c' split_by_close c s'
 
 
@@ -32,27 +32,25 @@ split_by_close = \s -> case s of
 str_to_strtree :: String -> Maybe [StrTree]
 str_to_strtree = \s -> case s of     --例のMaybe monad の話で大分綺麗になる気がする
     []     -> (Just [])
-    '(':s' -> case split_by_close s' of
-      Nothing 　　　　　-> Nothing
-      Just split_pair  -> case str_to_strtree (fst split_pair) of --括弧の中
-        Nothing          -> Nothing
-        Just inside_tree -> case str_to_strtree (snd split_pair) of     --括弧閉じの後ろ側
-          Nothing              -> Nothing
-          Just following_trees -> (Just((Tree inside_tree) : following_trees))
-    x:s'   -> case str_to_strtree s' of
-      Nothing -> Nothing
-      Just t  -> (Just((Chr x):t))
+    '(':s' -> let inside_tree     = (str_to_strtree =<< (fst <$> (split_by_close s')))  --括弧の中
+                  following_trees = (str_to_strtree =<< (snd <$> (split_by_close s'))) in  --括弧閉じの後ろ側
+                    ((\i f -> ((Tree i):f)) <$> (inside_tree)) <*> following_trees
+    x:s'   -> ((Chr x):) <$> (str_to_strtree s')
+
 
 strtree_to_str::StrTree -> String --デバッガ
 strtree_to_str = \t -> case t of
-  Tree t' -> ("("++(to_str_each strtree_to_str t')++")")
+  Tree t' -> ("<"++(strtree_to_str =<< t')++">")
   Chr  c  -> [c]
 
+
+
+{-    結局=<<そのものだった
 to_str_each :: (a -> String) -> [a] -> String  --デバッガ補助
 to_str_each = \f vec -> case vec of        --listをmonadとして使えばよいのでは？？
   []   -> []
   x:v' -> ((f x)++(to_str_each f v'))
-
+-}
 
 
 {-
